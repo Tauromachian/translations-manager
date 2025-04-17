d37d4a8e5d49234314640cb0c688e681701c9fc8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-17 19:29:24 -0400

    Initialize router

--- resources/main.js
+++ resources/main.js
@@ -1,4 +1,10 @@
+import { router } from "./services/router.js";
+import { routes } from "./services/routes.js";
+
 import "../styles/index.css";
 
 globalThis.addEventListener("DOMContentLoaded", () => {
+  appState.el = document.querySelector("#app");
+
+  router.init(routes, appState.el);
 });
