d4b1a64c1424ff66603527e5e34dcc19c221162e
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-17 20:45:12 -0400

    Add create button

--- resources/pages/CollectionPage.js
+++ resources/pages/CollectionPage.js
@@ -19,11 +19,16 @@ export class Collection extends HTMLElement {
                 margin-top: 5rem;
             }
 
+            .m-1 {
+                margin: 1rem;
+            }
+
             </style>
             <div class="container mt-5">
                 <h1>Collection</h1>
 
                 <div class="card">
+                    <button class="m-1">Create</button>
                     <table>
                         <thead>
                             <tr>
