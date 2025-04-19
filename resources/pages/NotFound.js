b8b89233979fba7435c7acbde8d6ca0e46e637d4
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-19 19:00:01 -0400

    Make basic 404 page

---
+++ resources/pages/NotFound.js
@@ -0,0 +1,37 @@
+export class NotFound extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const template = document.createElement("template");
+
+    template.innerHTML = `
+            <style>
+                div {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 8px;
+                    align-items: center;
+                    justify-content: center;
+                    height: 100%;
+                    min-height: 100vh;
+                }
+
+                h1 {
+                    font-size: 3rem;
+                }
+            </style>
+
+            <div>
+                <h1>404</h1>
+                <p>Page not found</p>
+                <app-button>
+                    Go Home
+                </app-button>
+            </div>
+        `;
+
+    this.appendChild(template.content.cloneNode(true));
+  }
+}
