f7fb5beb8d0eba6fb0ccc25d9cba04c79a773c7b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-19 20:51:47 -0400

    Implement regex routes

--- resources/services/router.js
+++ resources/services/router.js
@@ -1,9 +1,23 @@
 export const router = {
   routes: {},
+  regexRoutes: {},
   mainEl: null,
+
   init(routes, el) {
     this.routes = routes;
     this.mainEl = el;
+
+    for (const key in routes) {
+      if (Object.hasOwnProperty.call(routes, key)) {
+        if (key.includes(":")) {
+          const routeShards = key.split(":");
+          routeShards[1] = "(.*)";
+          this.regexRoutes[routeShards.join("")] = routes[key];
+        }
+      }
+    }
+
+    console.log(this.regexRoutes);
 
     globalThis.addEventListener("popstate", () => {
       this.go(event.state.route, false);
@@ -11,6 +25,23 @@ export const router = {
 
     this.go(location.pathname);
   },
+
+  getMatchingRegexRoutes(route) {
+    const matchingRoutes = [];
+
+    for (const key in this.regexRoutes) {
+      if (Object.hasOwnProperty.call(this.regexRoutes, key)) {
+        if (new RegExp(key).test(route)) {
+          matchingRoutes.push(this.regexRoutes[key]);
+        }
+      }
+    }
+
+    console.log(matchingRoutes);
+
+    return matchingRoutes;
+  },
+
   go(route, addToHistory = true) {
     if (addToHistory) {
       history.pushState({ route }, "", route);
@@ -24,6 +55,10 @@ export const router = {
 
     if (this.routes[route]) {
       pageElement = document.createElement(this.routes[route]);
+    } else if (this.getMatchingRegexRoutes.call(this, route).length) {
+      const matchingRegex = this.getMatchingRegexRoutes.call(this, route);
+      pageElement = document.createElement(matchingRegex[0]);
+      console.log(pageElement);
     } else {
       pageElement = document.createElement(this.routes["*"]);
     }
