7b97bd75c32f911e0414436e7c61df66ff808380
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-27 16:54:30 -0400

    Move collections logic into collections controller

---
+++ controllers/collections.controller.js
@@ -0,0 +1,76 @@
+import { db } from "../config/db.ts";
+
+import { eq, sql } from "drizzle-orm";
+
+import { collections as collectionsSchema } from "../database/schema/collections.ts";
+
+async function doFullTextSearch(query) {
+  const result = await db.execute(sql`
+        SELECT * 
+          FROM collections 
+          WHERE (setweight(to_tsvector(name), 'A') ||
+                 setweight(coalesce(to_tsvector(description), ''), 'B') 
+                 @@ to_tsquery(${query}))
+    `);
+
+  return result;
+}
+
+export async function index(req, res) {
+  const { search } = req.query;
+
+  let collections;
+
+  if (!search) {
+    collections = await db.select().from(collectionsSchema);
+  } else {
+    const result = await doFullTextSearch(search);
+    collections = result.rows;
+  }
+
+  res.status(200).json(collections);
+}
+
+export async function store(req, res) {
+  const { name, description } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "Name is required" });
+  }
+
+  const collection = await db
+    .insert(collectionsSchema)
+    .values({ name, description })
+    .returning();
+
+  res.json(collection);
+}
+
+export async function edit(req, res) {
+  const { name } = req.body;
+  const { id } = req.params;
+
+  if (!name) {
+    return res.status(400).json({ error: "Name is required" });
+  }
+
+  const collection = await db.insert(collectionsSchema).values({
+    ...req.body,
+    id,
+  })
+    .onConflictDoUpdate({
+      target: collectionsSchema.id,
+      set: { ...req.body, id },
+    })
+    .returning();
+
+  res.json(collection);
+}
+
+export async function destroy(req, res) {
+  const { id } = req.params;
+
+  await db.delete(collectionsSchema).where(eq(collectionsSchema.id, id));
+
+  res.status(204).send();
+}
--- routes/api.js
+++ routes/api.js
@@ -2,52 +2,15 @@ import express from "npm:express@5";
 
 export const router = express.Router();
 
-import { db } from "../config/db.ts";
-
-import { eq, sql } from "drizzle-orm";
-
-import { collections as collectionsSchema } from "../database/schema/collections.ts";
-
-async function doFullTextSearch(query) {
-  const result = await db.execute(sql`
-        SELECT * 
-          FROM collections 
-          WHERE (setweight(to_tsvector(name), 'A') ||
-                 setweight(coalesce(to_tsvector(description), ''), 'B') 
-                 @@ to_tsquery(${query}))
-    `);
-
-  return result;
-}
+import {
+  destroy,
+  edit,
+  index,
+  store,
+} from "../controllers/collections.controller.js";
 
 router.route("/collections")
-  .get(async (req, res) => {
-    const { search } = req.query;
-
-    let collections;
-
-    if (!search) {
-      collections = await db.select().from(collectionsSchema);
-    } else {
-      const result = await doFullTextSearch(search);
-      collections = result.rows;
-    }
-
-    res.status(200).json(collections);
-  }).post(async (req, res) => {
-    const { name, description } = req.body;
-
-    if (!name) {
-      return res.status(400).json({ error: "Name is required" });
-    }
-
-    const collection = await db
-      .insert(collectionsSchema)
-      .values({ name, description })
-      .returning();
-
-    res.json(collection);
-  });
+  .get(index).post(store);
 
 router.route("/collections/:id")
   .all((req, res, next) => {
@@ -58,28 +21,4 @@ router.route("/collections/:id")
     }
 
     next();
-  }).delete(async (req, res) => {
-    const { id } = req.params;
-
-    await db.delete(collectionsSchema).where(eq(collectionsSchema.id, id));
-
-    res.status(204).send();
-  }).put(async (req, res) => {
-    const { name } = req.body;
-    const { id } = req.params;
-
-    if (!name) {
-      return res.status(400).json({ error: "Name is required" });
-    }
-
-    const collection = await db.insert(collectionsSchema).values({
-      ...req.body,
-      id,
-    })
-      .onConflictDoUpdate({
-        target: collectionsSchema.id,
-        set: { ...req.body, id },
-      });
-
-    res.status(201).json(collection);
-  });
+  }).delete(destroy).put(edit);
