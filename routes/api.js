618f6c6659ebb8f2fcee2f92e536dd954b980b18
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-27 20:00:47 -0400

    Use new way to define controllers

--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,11 @@ import express from "npm:express@5";
 
 export const router = express.Router();
 
-import {
-  destroy,
-  edit,
-  index,
-  store,
-} from "../controllers/collections.controller.js";
+import * as collectionsController from "../controllers/collections.controller.js";
+import * as languagesController from "../controllers/languages.controller.js";
 
 router.route("/collections")
-  .get(index).post(store);
+  .get(collectionsController.index).post(collectionsController.store);
 
 router.route("/collections/:id")
   .all((req, res, next) => {
@@ -21,4 +17,18 @@ router.route("/collections/:id")
     }
 
     next();
-  }).delete(destroy).put(edit);
+  }).delete(collectionsController.destroy).put(collectionsController.edit);
+
+router.route("/languages")
+  .get(languagesController.index).post(languagesController.store);
+
+router.route("/languages/:id")
+  .all((req, res, next) => {
+    const { id } = req.params;
+
+    if (!id || Number.isNaN(Number(id))) {
+      return res.status(400).json({ error: "ID should be a number" });
+    }
+
+    next();
+  }).delete(languagesController.destroy).put(languagesController.edit);
