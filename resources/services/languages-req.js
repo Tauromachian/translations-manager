955cf1f5404cbe4842cdf9ae5c8393ec976795d0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-27 20:00:53 -0400

    Make languages req service

---
+++ resources/services/languages-req.js
@@ -0,0 +1,44 @@
+const baseUrl = "/api/languages";
+
+export async function getLanguages(searchText) {
+  let url = baseUrl;
+
+  if (searchText) {
+    url += `?search=${searchText}`;
+  }
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
+
+export async function postLanguage(data) {
+  const response = await fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+}
+
+export async function putLanguage(id, data) {
+  const response = await fetch(`${baseUrl}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+}
+
+export async function deleteLanguage(id) {
+  await fetch(`${baseUrl}/${id}`, {
+    method: "DELETE",
+  });
+}
