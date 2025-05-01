0a6329579663b671bb7e3bbd0b94a2840ce6bb07
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-05-01 14:22:26 -0400

    Remove unnecessary full text search

--- controllers/languages.controller.js
+++ controllers/languages.controller.js
@@ -1,32 +1,11 @@
 import { db } from "../config/db.ts";
 
-import { eq, sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 
 import { languages as languagesSchema } from "../database/schema/languages.ts";
 
-async function doFullTextSearch(query) {
-  const result = await db.execute(sql`
-        SELECT * 
-          FROM languages 
-          WHERE (setweight(to_tsvector(name), 'A') ||
-                 setweight(coalesce(to_tsvector(code), ''), 'B') 
-                 @@ to_tsquery(${query}))
-    `);
-
-  return result;
-}
-
-export async function index(req, res) {
-  const { search } = req.query;
-
-  let languages;
-
-  if (!search) {
-    languages = await db.select().from(languagesSchema);
-  } else {
-    const result = await doFullTextSearch(search);
-    languages = result.rows;
-  }
+export async function index(_, res) {
+  const languages = await db.select().from(languagesSchema);
 
   res.status(200).json(languages);
 }
