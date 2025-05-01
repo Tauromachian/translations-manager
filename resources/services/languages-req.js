a3b20bab48fb2e17404ee9ae646959393bce87cc
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-05-01 15:29:10 -0400

    Add filters to url

--- resources/services/languages-req.js
+++ resources/services/languages-req.js
@@ -1,7 +1,15 @@
+import { toQueryString } from "../../utilities/query_string.js";
+
 const baseUrl = "/api/languages";
 
-export async function getLanguages() {
-  const response = await fetch(baseUrl);
+export async function getLanguages(options) {
+  let url = baseUrl;
+
+  if (options) {
+    url += `?${toQueryString(options)}`;
+  }
+
+  const response = await fetch(url);
   const data = await response.json();
 
   return data;
