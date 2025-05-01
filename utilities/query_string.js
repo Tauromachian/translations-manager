5673dd2c08a00d75bdb3f5ad6646db221a520efb
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-05-01 16:34:40 -0400

    Handle arrays with query string

--- utilities/query_string.js
+++ utilities/query_string.js
@@ -1,5 +1,17 @@
 export function toQueryString(obj) {
   return Object.keys(obj)
-    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
+    .map((key) => {
+      if (Array.isArray(obj[key])) {
+        return obj[key]
+          .map((item) => {
+            return `${encodeURIComponent(key)}[]=${encodeURIComponent(item)}`;
+          })
+          .join("&");
+      }
+
+      {
+        return `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`;
+      }
+    })
     .join("&");
 }
