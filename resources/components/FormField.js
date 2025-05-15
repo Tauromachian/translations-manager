d0e19b9590bc0007bbb83d666b573b375b3d967c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-05-14 20:54:58 -0400

    Add disabled attribute

--- resources/components/FormField.js
+++ resources/components/FormField.js
@@ -13,7 +13,7 @@ export class FormField extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ["label", "name", "required", "apppend-inner-icon"];
+    return ["label", "name", "required", "apppend-inner-icon", "disabled"];
   }
 
   get value() {
