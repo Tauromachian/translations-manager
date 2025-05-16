cc426de4c49f84946135f81424e5e89342c12fe5
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-05-15 20:52:14 -0400

    Add basic loading state for the button

--- resources/components/AppButton.js
+++ resources/components/AppButton.js
@@ -1,3 +1,5 @@
+import "./AppLoader.js";
+
 export class AppButton extends HTMLElement {
   #button;
 
@@ -7,7 +9,25 @@ export class AppButton extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ["to", "type", "color"];
+    return ["to", "type", "color", "loading"];
+  }
+
+  attributeChangedCallback(name, _, newValue) {
+    if (!this.#button) return;
+
+    if (name === "loading") {
+      const loader = this.#button.querySelector(".button-loader");
+
+      const isLoading = newValue === "true" || newValue === "";
+
+      loader.style.display = isLoading ? "flex" : "none";
+
+      if (isLoading) {
+        this.#button.classList.add("button--loading");
+      } else {
+        this.#button.classList.remove("button--loading");
+      }
+    }
   }
 
   makeButton() {
@@ -24,7 +44,9 @@ export class AppButton extends HTMLElement {
 
     const button = document.createElement("button");
     button.setAttribute("class", "button");
-    button.innerHTML = `<slot></slot><span class="loader"></span>`;
+    button.innerHTML = `<slot></slot>
+      <span class="button-loader"><app-loader width="20px" height="20px"></app-loader></span>
+    `;
 
     if (type) {
       button.setAttribute("type", type);
@@ -78,6 +100,7 @@ export class AppButton extends HTMLElement {
                     border: none;
                     border-radius: 4px;
                     cursor: pointer;
+                    position: relative;
                     transition: background-color 0.2s ease;
                     font-size: 0.9rem;
                     display: block;
@@ -103,9 +126,30 @@ export class AppButton extends HTMLElement {
                     background-color: var(--danger);
                 }
 
+                .button--loading {
+                    pointer-events: none;
+                    cursor: not-allowed;
+                }
+
                 .button a {
                     text-decoration: none;
                     color: var(--black);
+                }
+
+                .button-loader {
+                    border-radius: 4px;
+                    background-color: var(--primary);
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    bottom: 0;
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    display: none;
                 }
             </style>
         `;
