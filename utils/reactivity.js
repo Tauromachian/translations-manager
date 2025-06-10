50f594c819ed50714540352c18c1c720d4d9cfe8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-06-09 20:41:05 -0400

    Fix/ Stop calling watchers if I am adding a watcher

--- utils/reactivity.js
+++ utils/reactivity.js
@@ -3,6 +3,8 @@ export function reactive(objectToWrap) {
     set(target, property, value) {
       target[property] = value;
 
+      if (property === "_watchers") return true;
+
       if (target?._watchers?.length) {
         target._watchers.forEach((callback) => callback(value));
       }
