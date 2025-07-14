47310ec327c6f9f4a48229fe391d02e05f49660b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-07-14 19:53:22 -0400

    Add funcionality to toggle dark theme

--- src/site/main.js
+++ src/site/main.js
@@ -1,3 +1,18 @@
 import "./styles/index.css";
 
 import "iconify-icon";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonEl = document.querySelector("header button");
+  const htmlEl = document.querySelector("html");
+
+  buttonEl.addEventListener("click", () => {
+    const htmlElDarkAttr = htmlEl.getAttribute("data-theme");
+
+    if (htmlElDarkAttr) {
+      htmlEl.removeAttribute("data-theme");
+    } else {
+      htmlEl.setAttribute("data-theme", "dark");
+    }
+  });
+});
