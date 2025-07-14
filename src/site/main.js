f333461794fc2b93805b83c4c81067dba6025552
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-07-14 19:57:46 -0400

    Make toggleTheme function

--- src/site/main.js
+++ src/site/main.js
@@ -2,17 +2,24 @@ import "./styles/index.css";
 
 import "iconify-icon";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const buttonEl = document.querySelector("header button");
+function toggleTheme() {
   const htmlEl = document.querySelector("html");
 
-  buttonEl.addEventListener("click", () => {
-    const htmlElDarkAttr = htmlEl.getAttribute("data-theme");
+  let htmlElDarkAttr = htmlEl.getAttribute("data-theme");
 
-    if (htmlElDarkAttr) {
-      htmlEl.removeAttribute("data-theme");
-    } else {
-      htmlEl.setAttribute("data-theme", "dark");
-    }
-  });
+  if (htmlElDarkAttr) {
+    htmlEl.removeAttribute("data-theme");
+  } else {
+    htmlEl.setAttribute("data-theme", "dark");
+  }
+
+  htmlElDarkAttr = htmlEl.getAttribute("data-theme");
+
+  localStorage.setItem("theme", htmlElDarkAttr ?? "light");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonEl = document.querySelector("header button");
+
+  buttonEl.addEventListener("click", toggleTheme);
 });
