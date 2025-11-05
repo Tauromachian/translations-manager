0aa124e698739d3ef22a0150aacc676f7c8bd2f0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-11-05 16:06:11 +0100

    Make envVariables util

---
+++ src/server/utils/envVariables.js
@@ -0,0 +1,15 @@
+/**
+ * Search for the env variable provided as an argument. If the variable doesn't exist it throws an error.
+ * @param {string[]} environmentVariables - The array of environment variables names.
+ * @throws {Error} - If the env variable doesn't exist.
+ */
+export function enforceEnvironmentVariables(environmentVariables) {
+  for (const variable of environmentVariables) {
+    if (!Deno.env.get(variable)) {
+      const errMessage = `The ${variable} environment variable is required`;
+      console.error(errMessage);
+      console.trace();
+      throw new Error(errMessage);
+    }
+  }
+}
