8bce34f6181cba79a157e77f1d8c6731c1721de4
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-12-17 16:42:49 +0100

    Add error handler

---
+++ src/server/middlewares/errorHandler.js
@@ -0,0 +1,50 @@
+import z from "zod";
+
+import { getLogger } from "@logtape/logtape";
+
+const logger = getLogger([]);
+
+function getMessageByCode(statusCode) {
+  if (statusCode >= 500) return "Internal Server Error";
+  if (statusCode == 404) return "Not Found";
+  if (statusCode == 403) return "Forbidden";
+  if (statusCode == 401) return "Unauthorized";
+  if (statusCode == 400) return "Bad Request";
+
+  return "Something went wrong";
+}
+
+export function errorHandler(err, req, res, _) {
+  logger.error(err);
+
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode);
+
+  const prodErrorObject = {
+    message: getMessageByCode(statusCode),
+  };
+
+  const devErrorObject = {
+    message: err.message || getMessageByCode(statusCode),
+    url: req.originalUrl,
+    method: req.method,
+    body: req.body,
+    user: req.user?.id || "unauthenticated",
+    cause: err.cause ?? "",
+  };
+  if (err.stack) devErrorObject.stack = err.stack;
+
+  if (statusCode === 400) {
+    err = z.treeifyError(err);
+
+    prodErrorObject.details = err;
+    devErrorObject.details = err;
+  }
+
+  if (Deno.env.get("NODE_ENV") === "development") {
+    return res.json(devErrorObject);
+  } else {
+    return res.json(prodErrorObject);
+  }
+}
