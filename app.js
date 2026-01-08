2b8e17f22179fb55baceee2a0f14565466aa835c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-01-08 20:38:48 +0100

    Ref/ Move rate limiter into its own file at middlewares

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import express from "express";
 import { load } from "std/dotenv";
 
 import morgan from "morgan";
-import { rateLimit } from "express-rate-limit";
 import swaggerUi from "swagger-ui-express";
 
 import { start as livereloadStart } from "./config/livereload.ts";
@@ -15,15 +14,9 @@ import { router as siteRouter } from "./routes/site.js";
 import { router as appRouter } from "./routes/app.js";
 import { router as apiRouter } from "./routes/api.js";
 import { errorHandler } from "@/server/middlewares/errorHandler.js";
+import { rateLimiter } from "@/server/middlewares/rateLimiter.js";
 
 const app = express();
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 100,
-  standardHeaders: "draft-8",
-  legacyHeaders: false,
-});
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -37,7 +30,7 @@ app.use("/app", appRouter);
 app.use("/api", apiRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use(limiter);
+app.use(rateLimiter);
 
 app.use((_, _1, next) => {
   next({ statusCode: 404 });
