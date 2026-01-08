d447a0cfab603ebacb221971b59a7a09f55d89df
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-01-08 20:27:31 +0100

    Remove unnecessary wildcard

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(limiter);
 
-app.use("*", (_, _1, next) => {
+app.use((_, _1, next) => {
   next({ statusCode: 404 });
 });
 
