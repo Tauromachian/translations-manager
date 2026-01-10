36659eb74d3ec9665aabcc3cf363ac3dd7d13b8c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-01-10 09:48:09 +0100

    Use a generated column to calculate search vector

--- database/schema/collections.ts
+++ database/schema/collections.ts
@@ -1,14 +1,30 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
-import { relations } from "drizzle-orm";
+import { customType, index, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { relations, sql } from "drizzle-orm";
 
 import { translations } from "./translations.ts";
 import { languages } from "./languages.ts";
+
+const tsVector = customType<{ data: string }>({
+  dataType() {
+    return "tsvector";
+  },
+});
 
 export const collections = pgTable("collections", {
   id: serial().primaryKey().notNull(),
   name: text("name").notNull(),
   description: text("description"),
-});
+  searchVector: tsVector("search_vector").generatedAlwaysAs(() =>
+    sql`
+        SELECT * 
+          FROM collections 
+          WHERE (setweight(to_tsvector(name), 'A') ||
+                 setweight(coalesce(to_tsvector(description), ''), 'B') 
+        `
+  ),
+}, (table) => [
+  index("index_search_vector").using("gin", table.searchVector),
+]);
 
 export const collectionsRelations = relations(collections, ({ many }) => ({
   translations: many(translations),
