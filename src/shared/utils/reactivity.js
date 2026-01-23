b85ff3068807788fbcfb47a90697247a79fc682a
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-01-23 21:15:40 +0100

    Add computed function

--- src/shared/utils/reactivity.js
+++ src/shared/utils/reactivity.js
@@ -1,15 +1,21 @@
+let activeEffect = null;
+
 export function reactive(objectToWrap) {
   objectToWrap["_watchers"] = [];
 
   return new Proxy(objectToWrap, {
+    get(target, property) {
+      if (!target._watchers.includes(activeEffect)) {
+        target._watchers.push(activeEffect);
+      }
+
+      return target[property];
+    },
     set(target, property, value) {
       target[property] = value;
 
       if (property === "_watchers") return true;
-
-      if (target?._watchers?.length) {
-        target._watchers.forEach((callback) => callback(value));
-      }
+      target._watchers.forEach((callback) => callback(value));
 
       return true;
     },
@@ -22,4 +28,22 @@ export function ref(value) {
 
 export function watch(ref, callback) {
   ref._watchers.push(callback);
+}
+
+export function computed(callback) {
+  const computedObj = {
+    _watchers: [],
+    get value() {
+      const value = callback();
+
+      this._watchers.forEach((callback) => callback(value));
+
+      return value;
+    },
+  };
+
+  activeEffect = () => computedObj.value;
+  computedObj.value;
+
+  return computedObj;
 }
