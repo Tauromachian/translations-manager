47a6d4b1128a936644aa70358a1a7806791cc415
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-05 20:48:07 +0100

    Add return type annotation

--- src/shared/utils/rendering.js
+++ src/shared/utils/rendering.js
@@ -1,6 +1,7 @@
 /**
  * @param {string} elName - Tag name of HTML element to create
  * @param {Object.<string, (string|number)>} props - Object containing attributes you want to put into the HTML element
+ * @returns {HTMLElement}
  */
 export function makeEl(elName, props) {
   const newEl = document.createElement(elName);
