57c3f6e47ee5e761ca1cc8cc5f0f587e1507e657
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-05 21:05:29 +0100

    Add type annotation of children param

--- src/shared/utils/rendering.js
+++ src/shared/utils/rendering.js
@@ -1,6 +1,7 @@
 /**
  * @param {string} elName - Tag name of HTML element to create
  * @param {Object.<string, (string|number)>} props - Object containing attributes you want to put into the HTML element
+ * @param {Array.<HTMLElement>} children
  * @returns {HTMLElement}
  */
 export function makeEl(elName, props, children) {
